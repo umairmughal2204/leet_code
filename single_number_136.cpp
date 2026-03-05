9ec03f24381b147df125798fbdf65bacfb5ebff0
umairmughal2204 <03104676590umary@gmail.com>
2026-03-06 03:01:27 +0500

    solving single number problem 136 using xor trick

--- single_number_136.cpp
+++ single_number_136.cpp
@@ -4,34 +4,43 @@
 
 using namespace std;
 
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+//         unordered_map<int,int> freq;
+
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             freq[nums[i]]++;
+//         }
+
+//         for(auto it:freq)
+//         {
+//             if(it.second==1)
+//                 return it.first;
+//         }
+
+//         return -1;
+//     }
+// };
+
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int> freq;
+
+        int ans = 0;
 
         for(int i=0;i<nums.size();i++)
         {
-            freq[nums[i]]++;
+            ans = ans ^ nums[i];
         }
 
-        for(auto it:freq)
-        {
-            if(it.second==1)
-                return it.first;
-        }
-
-        return -1;
+        return ans;
     }
 };
 
 int main() {
-    int n;
-    cin >> n;
-
-    vector<int> nums(n);
-    for (int i = 0; i < n; i++) {
-        cin >> nums[i];
-    }
+    vector<int> nums = {4, 1, 2, 1, 2};
 
     Solution sol;
     cout << sol.singleNumber(nums) << endl;
